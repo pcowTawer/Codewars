0219f18001b689806760ac7990bb6f63d6a50bb4
Ilya_Prostakov <prostocowtawer@gmail.com>
2022-04-17 01:03:21 +0700

    MySolution

--- 6/main.c
+++ 6/main.c
@@ -11,16 +11,41 @@
 // Empty list is considered to have zero greatest sum. 
 // Note that the empty list or array is also a valid sublist/subarray.
 #include <stddef.h>
+#include <stdio.h>
 
-int maxSequence(const int* array, size_t n) {
+int maxSequence(const int* array, size_t n) 
+{
+    int i = 0, next_max = 0, max = 0, positive = 0, counter = 0, buf = 0;
 
-    // <---- hajime!
-
+    while (buf < n)
+    {
+        positive = 0; next_max = 0; counter = 0;
+        for(i = buf; array[i] <= 0; ++i);
+        for(; i < n; ++i)
+        {
+            if (array[i] > 0) positive+=array[i];
+            if (array[i] < 0) break; 
+        }
+        for(; array[i] <= 0; ++i)
+        {
+            counter += array[i];
+            if (counter > next_max) next_max = counter;
+        }
+        buf = i;
+        for(; i < n; ++i)
+        {
+            counter += array[i];
+            if (counter > next_max) next_max = counter;
+        }
+        if (positive + next_max > max) max = positive + next_max;
+    }
+    return max;
 }
 
 int main()
 {
-    const int array[] = {};
-    maxSequence(array, 0);
+    const int array[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    fprintf(stdout, "%d", maxSequence(array, 9));
+    
     return 0;
 }
